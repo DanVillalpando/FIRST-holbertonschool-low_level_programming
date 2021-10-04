dce72854b1cf2fcd614d60d63b29412fc7a05c9b
DanVillalpando <danielvch5@gmail.com>
2021-10-03 18:07:15 -0700

    c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,12 +4,12 @@
 int main/*Main with void*/(void)
 {
 int numbz = '0';
-for (numbz <= '9')
+while (numbz <= '9')
 {
 putchar(numbz);
 numbz++;
 }
-putchar(numbz);
+putchar('\n');
 /*Does the same but with putchar*/
 return (0);
 /*Return zero*/
