8479fc1fadc63eb3125bfea05cd72a177f7d6a8e
DanVillalpando <danielvch5@gmail.com>
2021-10-04 14:07:46 -0700

    00

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,15 +9,6 @@ int li = '4';
 int lli = '8';
 int f = '4';
 /*variables*/
-for (32bit machine)
-{
-printf("Size of a char: % d byte(s)\n", c);
-printf("Size of an int: % d byte(s)\n", i);
-printf("Size of a long int: % d byte(s)\n", li);
-printf("Size of a long long int: % d byte(s)\n", lli);
-printf("Size of a float: % d byte(s)\n", f);
-}
-for (64bit machine)
 printf("Size of a char: % d byte(s)\n", c);
 printf("Size of an int: % d byte(s)\n", i);
 printf("Size of a long int: % d byte(s)\n", li);
