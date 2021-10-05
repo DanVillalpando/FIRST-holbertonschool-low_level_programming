8c646f0ec9abde9198f415f19f577f5353127109
DanVillalpando <danielvch5@gmail.com>
2021-10-05 15:09:33 -0700

    03

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,10 +5,13 @@ int main/*Main function*/(void)
 {
 char c[8] = "_putchar";
 int s = 0;
-for (int s = 0; s < 8; s++)
+if (s == 0)
 {
+do {
 putchar(c[s]);
+s++;
 }
+while (s == 8) }
 /*Just print*/
 return (0);
 /*Return zero*/
