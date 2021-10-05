1c2ac0044b5e65ecd35f9ff623546ae5133e1976
DanVillalpando <danielvch5@gmail.com>
2021-10-04 19:17:24 -0700

    00

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 int main/*Main function*/(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 /*Print the message*/
 return (0);
 /*Returns zero*/
