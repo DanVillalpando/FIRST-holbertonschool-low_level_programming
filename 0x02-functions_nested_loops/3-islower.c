3ba1b74d0aa1c7b4e3e7c116bf7594f427529c65
DanVillalpando <danielvch5@gmail.com>
2021-10-05 20:40:35 -0700

    02

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+int _islower(int c);
+int main(void)
+{
+  char c;
+  int i;
+    for (int i = 0; c[i]; i++)
+          {
+            putchar(islower[]);
+          }
+        putchar('\n');
+    return (0);
+}
