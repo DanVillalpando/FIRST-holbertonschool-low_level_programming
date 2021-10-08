425b62b5a5be8bf96f25c9b5982c861cefdf8db8
DanVillalpando <danielvch5@gmail.com>
2021-10-07 21:59:45 -0700

    003

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/*The headers*/
+
+int main/** main - remains the same*/()
+{
+int n;
+
+ if (n == 0)
+ {
+   printf("%d is zero\n", n);
+ }
+    else
+   {
+     if (n > 0)
+     {
+       printf("%d is positive\n", n);
+     }
+     else
+       {
+         printf("%d is negative\n", n);
+       }
+   }
+
+/*My code that checks positives, negative or zero*/
+return (0);
+/*return: Always 0*/
+}
+
