fbf3b012d604cae5ec2f9eb7f54e5d867bbb2aac
DanVillalpando <danielvch5@gmail.com>
2021-10-22 11:56:38 -0700

    0A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[])
+int main/*Arguments*/(int argc, char *argv[])
 {
   int i = 0;
-  for (i = 0; i < argc; i++)
-    printf ("%s\n", argv[i]);
+
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
   return (0);
 }
