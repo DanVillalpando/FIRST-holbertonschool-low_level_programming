0ddc3c333c1cd7046bb9d6090653f64d2a1f79b3
DanVillalpando <danielvch5@gmail.com>
2021-10-28 15:41:45 -0700

    0C

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,11 +6,11 @@ int main/*Arguments 2*/(int argc, char *argv[])
   int i = 0;
   if (argv[i] != 0)
     {
-for (i = 0; i < argc; i++)
-  {
-    argc--;
-    printf("%d\n", argc);
-  }
+      for (i = 0; i < argc; i++)
+	{
+	  argc--; 
+	}
+    printf("%d\n", argc);		
     }
- return (0);
+  return (0);
 }
