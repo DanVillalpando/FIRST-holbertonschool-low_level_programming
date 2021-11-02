a91a6b19d1f2541ab703842b28b53eb98fe738cf
DanVillalpando <danielvch5@gmail.com>
2021-11-01 22:45:38 -0700

    0D

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,11 @@
+#ifndef __HEADER__
+#define __HEADER__
+void init_dog(struct dog *d, char *name, float age, char *owner)
+/*First struct*/struct/*Dog info*/ dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
+#if
