76b40729c8cf725861c20ffd7ea99fc7ed965574
DanVillalpando <danielvch5@gmail.com>
2021-11-01 21:39:51 -0700

    0D

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,13 @@
+#include <stdlib.h>
 #ifndef __HEADER__
 #define __HEADER__
 #define ABS(x) (x)
+#if ABS <= 1
+int x;
+int x = abs(ABS(x));
+printf("%d\n", x);
+#endif
+#if ABS(x) == 0
+printf("0");
+#endif
 #endif
