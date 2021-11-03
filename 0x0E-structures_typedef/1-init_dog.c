410c37ed052aecebae9c1b4982131621ea52bdee
DanVillalpando <danielvch5@gmail.com>
2021-11-02 19:51:17 -0700

    E

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,3 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+  struct dog
+{
+};
+  
+return (n * factorial(n - 1));
+}
+
+
+#endif /* MAIN_H */
+
 #ifndef __HEADER__
 #define __HEADER__
 struct dog
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,12 @@
 #ifndef __HEADER__
 #define __HEADER__
-void init_dog(struct dog *d, char *name, float age, char *owner)
+#include <stdio.h>
+
 /*First struct*/struct/*Dog info*/ dog
 {
 char *name;
 float age;
 char *owner;
 };
+typedef struct dog dog;
 #endif
-#if
