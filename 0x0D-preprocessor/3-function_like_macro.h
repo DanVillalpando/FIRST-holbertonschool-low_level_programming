d1ce024f403dd6c10a01fbfbcd537b22f3532e42
DanVillalpando <danielvch5@gmail.com>
2021-11-04 14:34:58 -0700

    0D

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,6 @@
-#include <stdlib.h>
 #ifndef __HEADER__
 #define __HEADER__
-#define ABS(x) (x)
-#if ABS <= 1
-int x;
-int x = abs(ABS(x));
-printf("%d\n", x);
-#endif
-#if ABS(x) == 0
-printf("0");
-#endif
+
+#define ABS(x) (((x) <= (0)) ? (-x) : (x))
+
 #endif
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,7 +3,7 @@
 #include "dog.h"
 /***
  *Free the dogs
- *xc
+ *
  */void free_dog(dog_t *d)
 {
 free(d);
