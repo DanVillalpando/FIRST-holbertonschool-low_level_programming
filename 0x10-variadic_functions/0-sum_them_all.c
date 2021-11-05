95932d6a9250c8c095808775131b6689d6f04da7
DanVillalpando <danielvch5@gmail.com>
2021-11-05 13:18:31 -0700

    10

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,8 +4,12 @@
 int sum_them_all(const unsigned int n, ...)
 {
   unsigned int n;
-
+  int suma, i;
+  
   if (n == 0)
     return (0);
-  
+  for(i=0; i<n; i++)
+    {
+      suma = suma + n[i];
+    }   
 }
---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "variadic_functions.h"x
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+  if (separator == NULL)
+    return (0);
+}
