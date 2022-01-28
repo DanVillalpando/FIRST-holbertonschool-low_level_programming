fa0635a061d9134fbb8e1be99c7b9d8640695d68
DanVillalpando <danielvch5@gmail.com>
2022-01-28 11:38:12 -0800

    Correction of 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,24 +8,24 @@
  * or NULL if wrong
  */hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
-	unsigned long int i;
-	if (size == 0)
-		return (NULL);
+hash_table_t *new_table;
+unsigned long int i;
+if (size == 0)
+return (NULL);
 
-	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL)
-		return (NULL);
+new_table = malloc(sizeof(hash_table_t));
+if (new_table == NULL)
+return (NULL);
 
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
+new_table->array = malloc(sizeof(hash_node_t *) * size);
 
-	if (new_table->array == NULL)
-		return (NULL);
+if (new_table->array == NULL)
+return (NULL);
 
-	new_table->size = size;
+new_table->size = size;
 
-	for (i = 0; i < size; i++)
-		new_table->array[i] = NULL;
+for (i = 0; i < size; i++)
+new_table->array[i] = NULL;
 
-	return (new_table);
+return (new_table);
 }
