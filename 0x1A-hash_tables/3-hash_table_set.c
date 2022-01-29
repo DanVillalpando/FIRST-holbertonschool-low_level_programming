df4cc531f55fb4778ca9c1c2dba6a5a6fff980ca
DanVillalpando <danielvch5@gmail.com>
2022-01-28 20:08:32 -0800

    gcc errors fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include "hash_tables.h"
 
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+/***
+ *function that adds an element to the hash table
+ */int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
 	hash_node_t *ptr, *tmp;
@@ -11,11 +13,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		return (0);
 	}
-	index = key_index((const unsigned char *)key, ht->size);	#index of key
+	index = key_index((const unsigned char *)key, ht->size);
 	ptr = ht->array[index];
 	if (ptr == NULL)
 	{
-		ht->array[index] = create_node(key, value);
+		ht->array[index] = create_new_node(key, value);
 		return (1);
 	}
 	while (ptr != NULL)
@@ -31,6 +33,27 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		tmp = ptr;
 		ptr = ptr->next;
 	}
-	tmp->next = create_node(key, value);
+	tmp->next = create_new_node(key, value);
 	return (1);
+}
+
+/***
+* create a new hash_node_t
+*@key: const char
+*@value: const char
+*/hash_node_t *create_new_node(const char *key, const char *value)
+{
+        hash_node_t *node;
+
+        node = malloc(sizeof(hash_node_t));
+        if (node == NULL)
+                return (0);
+        node->key = strdup(key);
+        if (node->key == NULL)
+                return (0);
+        node->value = strdup(value);
+        if (node->value == NULL)
+                return (0);
+        node->next = NULL;
+        return (node);
 }
